1a4fe73854930f7fc92da4db7cc5261e49ca71fe
Loganwebb1994 <loganwebb1994@gmail.com>
2021-05-10 09:31:48 -0500

    added games to views

--- levelup/urls.py
+++ levelup/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from rest_framework import routers
-from levelupapi.views import GameTypes
+from levelupapi.views import GameTypes, Games
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
@@ -22,6 +22,7 @@ from levelupapi.views import register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
+router.register(r'games', Games, 'game')
 
 
 urlpatterns = [
